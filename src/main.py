fa3f94ce20ad511eb64be1a3e8b256f8e7eb96bc
Shiva Nadi <s.nadi@uu.nl>
2021-05-05 08:39:17 +0200

    initial commit

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sentinews.preprocessing import clean_text as pp
+from sentinews.wordvector import generate_word2vec as wtv
+
+def main_function(operation):
+    if operation == 'preprocessing':
+        # Read the original dataset
+        df = pd.read_csv('../data/raw/news-dataset--2010-04-21.csv')
+
+        # clean dataset including removing stopwords, html tags, newlines, and drop nulls
+        pp.clean(df)
+
+
+    if operation == 'word2vec':
+        df = pd.read_csv('../data/processed/news-dataset--2010-04-21.csv')
+        wtv.train_word2vec_model(df)
+
+    if operation == 'most similarity':
+        wtv.check_most_similar('islam', 5)
+
+    if operation == 'check similarites':
+        wtv.check_similarities('corona', 'vaccin')
+
+if __name__=='__main__':
+    main_function('check similarites')
