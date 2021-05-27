77ca5ab3a7fd80329b9db9d1f9546bb84c380e5a
Shiva Nadi <s.nadi@uu.nl>
2021-05-27 12:57:05 +0200

    two function added for calculating the sentiment based on sentences

--- src/sentinews/sentiment/calculate_sentiment.py
+++ src/sentinews/sentiment/calculate_sentiment.py
@@ -2,6 +2,7 @@ from gensim.models import Word2Vec
 import pandas as pd
 from scipy import spatial
 import gensim.models.keyedvectors as word2vec
+import nltk
 from sklearn.metrics.pairwise import cosine_similarity
 
 def sentiment_calc(df):
@@ -46,9 +47,65 @@ def sentiment_calc_pretrained(df):
     print(df)
     df.to_csv('../../../data/processed/news_sentiment_pretrained.csv', index=False)
 
+# function ss(sentence s)
+# {
+# a = wordvec(good)
+# b = wordvec(bad)
+#
+# sum = 0
+# for each word w in sentence s:
+# 	sw = cos(w,a)-cos(w,b)
+# 	sum +=sw
+#
+# return sum
+# }
+#
+# # cos = PMI
+#
+# function sa(article d)
+# {
+# tokenize sentence(d)
+# sum = 0
+# for each sentence s in article d:
+#   	sum += ss(s)
+# return sum
+# }
+
+def sentiment_sentence_to_word(sentence):
+    # Load word vectore for word 'goed'
+    model = Word2Vec.load("../../../results/models/word2vec.model")  # Load model
+    vector_pos = model.wv['goed']  # Get numpy vector of word 'goed'
+    vector_neg = model.wv['slecht']  # Get numpy vector of word 'slecht'
+
+    sum =0
+    for word in sentence.split():
+        if word in model.wv.vocab:
+            word_vector = model.wv[word]
+            sw_pos = 1 - spatial.distance.cosine(word_vector, vector_pos)
+            sw_neg = 1 - spatial.distance.cosine(word_vector, vector_neg)
+            sum += sw_pos - sw_neg
+    return sum
+
+def sentiment_calc_sentence(df):
+    for i in range(len(df)):
+        sum =0
+        sentence = nltk.tokenize.sent_tokenize(df.loc[i,'text'])
+        for s in sentence:
+            sum+=sentiment_sentence_to_word(s)
+        df.loc[i,'score_sentence']=sum
+
+
+
 
 if __name__=='__main__':
     df = df = pd.read_csv('../../../data/processed/news_sentiment.csv')
-    df.dropna(subset=['clean_text'], inplace=True)
-    df.reset_index(drop=True, inplace=True)
-    sentiment_calc_pretrained(df)
+    # df.dropna(subset=['clean_text'], inplace=True)
+    # df.reset_index(drop=True, inplace=True)
+    # sentiment_calc_pretrained(df)
+    df = df.head(5)
+
+    sentiment_calc_sentence(df)
+    print(df)
+
+
+
