07d11148f91e13a324b836b5d0b91f8629288134
Shiva Nadi <s.nadi@uu.nl>
2021-06-08 14:09:56 +0200

    comment added

--- src/sentinews/wordvector/generate_word2vec.py
+++ src/sentinews/wordvector/generate_word2vec.py
@@ -6,6 +6,11 @@ from gensim.models import KeyedVectors
 
 
 def train_word2vec_model(df):
+    """
+    Builds wordvectors using gensim library on the clean_text column of the input dataset
+    :param df:
+    :type df: DataFrame
+    """
     df.dropna(subset=['clean_text'], inplace=True)
     sent = [row.split(' ') for row in df['clean_text']]
     model = Word2Vec(sent, min_count=1, size=100, workers=3, window=5, sg=1)
@@ -15,6 +20,14 @@ def train_word2vec_model(df):
         dill.dump(model, f)
 
 def check_most_similar(word, n):
+    """
+    find the most n similar words to 'word' in the vector space
+
+    :param word: The word we would like to find similar words
+    :type word: str
+    :param n: number of most similar words
+    :type n: int
+    """
     path = Path(__file__).parent / "../../../results/models/word2vec.model"
     with open (path, 'rb') as f:
         model = dill.load(f)
@@ -22,6 +35,13 @@ def check_most_similar(word, n):
     print(sims)
 
 def check_similarities(word1, word2):
+    """
+    finds similarity between wordvector 1 and wordvector 2
+    :param word1:
+    :type word1: str
+    :param word2:
+    :type word2: str
+    """
     path = Path(__file__).parent / "../../../results/models/word2vec.model"
     with open(path, 'rb') as f:
         model = dill.load(f)
@@ -43,12 +63,12 @@ if __name__ == '__main__':
     # model = Word2Vec.load("../../../results/models/word2vec.model")
     # sims = model.wv.most_similar('vaccin', topn=10)
     # print(sims)
-    # check_most_similar('slecht', 5)
+    check_most_similar('slecht', 5)
     # check_similarities('corona', 'vaccin')
 
     # Get numpy vector of a word
     # Load back with memory-mapping = read-only, shared across processes.
-    model = Word2Vec.load("../../../results/models/word2vec.model")
-    vector = model.wv['goed']  # Get numpy vector of a word
-    print(vector)
+    # model = Word2Vec.load("../../../results/models/word2vec.model")
+    # vector = model.wv['goed']  # Get numpy vector of a word
+    # print(vector)
 
