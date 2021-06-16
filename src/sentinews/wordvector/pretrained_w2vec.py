86eaebba4ecefaef132c5b5741b32de8821795b1
Shiva Nadi <s.nadi@uu.nl>
2021-06-16 11:23:49 +0200

    add resource of the model as comment

--- src/sentinews/wordvector/pretrained_w2vec.py
+++ src/sentinews/wordvector/pretrained_w2vec.py
@@ -1,7 +1,7 @@
 from gensim.models import Word2Vec
 import gensim.models.keyedvectors as word2vec
 
-
+# https://github.com/clips/dutchembeddings
 # model = Word2Vec.load_word2vec_format("../../../sentinews/pre-trained/wikipedia-320.txt",
 model = word2vec.KeyedVectors.load_word2vec_format("../../../pre-trained/wikipedia-160.txt", binary=False)
 katvec = model['goed']
