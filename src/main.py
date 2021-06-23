bb89e960b052e1aaebaed603607f500e81f01a0e
Shiva Nadi <s.nadi@uu.nl>
2021-06-23 13:29:57 +0200

    update in word2vec, sentiment and preprocessing sector

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sentinews.preprocessing import clean_text as pp
 from sentinews.wordvector import generate_word2vec as wtv
+from sentinews.sentiment import calculate_sentiment as st
 
 def main_function(operation, **kwargs):
     """
@@ -12,11 +13,12 @@ def main_function(operation, **kwargs):
     :param operation: Determines the type of operation
     :type operation: Str
     """
-    word = kwargs.get('word', None)
-    n = kwargs.get('n', None)
-    word1 = kwargs.get('word1', None)
-    word2 = kwargs.get('word2', None)
+    word = kwargs.get('word', None)    # Input parameter for function check_most_similar()
+    n = kwargs.get('n', None)          # Input parameter for function check_most_similar()
+    word1 = kwargs.get('word1', None)  # Input parameter for function check_similarities()
+    word2 = kwargs.get('word2', None)  # Input parameter for function check_similarities()
 
+    # ********************** Pre-processing *********************
     if operation == 'preprocessing':
         # Read the original dataset
         df = pd.read_csv('../data/raw/news-dataset--2021-05-11.csv') # Period of six month
@@ -24,7 +26,7 @@ def main_function(operation, **kwargs):
         # clean dataset including removing stopwords, html tags, newlines, and drop nulls
         pp.clean(df)
 
-
+    # ********************** Word2vec *********************
     if operation == 'word2vec':
         df = pd.read_csv('../data/processed/news-dataset--2021-05-11.csv')
         wtv.train_word2vec_model(df)
@@ -35,12 +37,24 @@ def main_function(operation, **kwargs):
     if operation == 'check similarity':
         wtv.check_similarities(word1, word2)
 
+    # ********************** Sentiment *********************
+    if operation == 'sentiment':
+        df = pd.read_csv('../data/processed/news-dataset--2021-05-11.csv')
+        df = df.head(5)
+        st.sentiment_words(df)
+
+from pathlib import Path
+
 if __name__=='__main__':
     # main_function('preprocessing') # Preprocess the original dataset (text column) and add it to clean_text column
 
     # main_function('word2vec') # Build wordvectors from the corpus
 
-    # main_function('similar words', word='islam', n=5) # find most n similar words to word w
+    main_function('similar words', word='immigratie', n=5) # find most n similar words to word w
 
-    main_function('check similarity', word1='corona', word2='vaccin') # find similarity between word1 and word2
+    # main_function('check similarity', word1='corona', word2='vaccin') # find similarity between word1 and word2
+
+    # main_function('sentiment') # find sentiment of each document in the dataset
+
+    # main_function('sentiment')
 
