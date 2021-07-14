4a9fa0b41c15f5f41f73f1a8196a7a8285f79325
Shiva Nadi <s.nadi@uu.nl>
2021-07-14 15:19:31 +0200

    filter_cities() function added

--- src/sentinews/filter_articles/filter.py
+++ src/sentinews/filter_articles/filter.py
@@ -12,7 +12,7 @@ import re
 #     if re.search(word, df.loc[0, 'text']):
 #         print('1')
 
-    # print(1 if re.search(word, df.loc[0, 'text']) else 0)
+# Filter OutGroups articles including any subset of the words present in the filter list
 def filter_art(df):
     df['out_group']=0
     for index, row in df.iterrows():
@@ -24,9 +24,28 @@ def filter_art(df):
             if re.search(word, df.loc[index, 'text']):
                 df.loc[index, 'out_group']=1
 
+# Filter articles including 'Nederland' or a name of cities or states in the Netherlands
+def filter_cities(df):
+    cities_df = pd.read_csv('../../../data/cites_list/cities.csv', sep=';')
+    cities_name = cities_df['Naam_2'].tolist()
+    states_name = cities_df['Naam_4'].tolist()
+    living_areas_filter = cities_name + states_name
+    living_areas_filter.append('Nederland')
+    living_areas_filter = set(living_areas_filter)
+    for index, row in df.iterrows():
+        if any(x in df.loc[index, 'text'] for x in living_areas_filter):
+            df.loc[index, 'netherlands'] = 1
+        else:
+            df.loc[index, 'netherlands'] = 0
+
+
+
 if __name__=='__main__':
     df = pd.read_csv('../../../data/processed/news-dataset--2021-05-11.csv')
     # df = df.head(10)
-    filter_art(df)
-    print(df[df['out_group']==1])
+    # filter_art(df)
+    # print(df[df['out_group']==1])
+    filter_cities(df)
+    df = df.head(10)
+    print(df['netherlands'])
 
