2a31bb02908a3f25f219483e3e100ee844b2db3e
Shiva Nadi <s.nadi@uu.nl>
2021-07-20 11:12:26 +0200

    header fixed

--- src/sentinews/filter_articles/filter.py
+++ src/sentinews/filter_articles/filter.py
@@ -1,8 +1,13 @@
+# Filter outgroups from the whole set of articles
+# Data provided by "online news database for a period of 8 months" : Start date: 10-11-2020   End date: 10-07-2020
+# Last edit on 20-07-2021 by "Shiva Nadi"
+
+
+# Packages
 import pandas as pd
 import re
 
 
-#
 # Exclude articles related to theater, books, films, art, and lifestyle
 def exclude_art(df):
     filter_art = ['theater', 'boek', 'film', 'kunst', 'roman']
@@ -10,7 +15,7 @@ def exclude_art(df):
         df.loc[index, 'art'] = 0
         if any(x in df.loc[index, 'text'] for x in filter_art):
             df.loc[index, 'art'] = 1
-    df = df[df['art']==0]
+    df = df[df['art'] == 0]
     return df
 
 
@@ -24,8 +29,9 @@ def filter_outgroups(df):
         for word in filter_list:
             if re.search(word, df.loc[index, 'text']):
                 df.loc[index, 'out_group'] = 1
-    df = df[df['out_group']==1]
+    df = df[df['out_group'] == 1]
     return df
+
 
 # Filter articles including 'Nederland' or a name of cities or states in the Netherlands
 def filter_cities(df):
@@ -41,12 +47,11 @@ def filter_cities(df):
         if any(x in df.loc[index, 'text'] for x in living_areas_filter):
             df.loc[index, 'netherlands'] = 1
 
-    df = df[df['netherlands']==1]
+    df = df[df['netherlands'] == 1]
     return df
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     df = pd.read_csv('../../../data/processed/news-dataset--2021-05-11.csv')
     print(df.shape)
 
